dc7383bd709b180658e002c3cd777631479e413e
Elton Murillo <elton.murillo@hey.com>
2023-01-07 14:31:37 -0800

    adds token class and token enums.

---
+++ Token.cs
@@ -0,0 +1,25 @@
+namespace CompilerLearning {
+    class Token {
+        // token typed specified by the enums.
+        readonly TokenType type;
+        // this is the text representation of the token.
+        readonly string lexeme;
+
+        // this could be a string, int or, double, etc.
+        readonly Object literal;
+        // line (in text file) the token is located.
+        readonly int line;
+
+
+        Token(TokenType type, String lexeme, Object literal, int line) {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+        
+        public override String ToString() {
+            return type + " " + lexeme + " " + literal;
+        }
+    }
+}
